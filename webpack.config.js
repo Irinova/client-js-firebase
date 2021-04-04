787a2a38ba044344ff0c4ed35988a3e5af7d0f7b
Irina <i.zakharova@informika.ru>
2021-04-04 13:07:03 +0300

    webpack

---
+++ webpack.config.js
@@ -0,0 +1,26 @@
+const path = require('path');
+
+module.exports = {
+    entry: './src/main.ts',
+    module: {
+        rules: [
+            {
+                test: /\.tsx?$/,
+                use: 'ts-loader',
+                exclude: /node_modules/,
+            },
+        ],
+    },
+    resolve: {
+        extensions: ['.tsx', '.ts', '.js'],
+    },
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'public'),
+    },
+    devtool: 'source-map',
+    devServer: {
+        contentBase: path.resolve(__dirname, 'public'),
+        watchContentBase: true,
+    },
+};
